ee6a2a39418f30a1b3df4c7b87d0fab22e0875cb
alexandre99 <alexanddy99@gmail.com>
2025-02-02 13:39:49 -0300

    Removed unnecessary files

--- delete-property.sh
+++
@@ -1,1 +0,0 @@
-curl -X DELETE http://localhost:8080/property/f1437f4a-6730-4eda-9685-4f23cc5dcd10
--- register-property.sh
+++
@@ -1,7 +0,0 @@
-curl -i -X POST http://localhost:8080/property \
-     -H "Content-Type: application/json" \
-     -d '{
-           "name": "Studio RM - Seafront!",
-           "ownerId": "6a598ba2-07c3-4036-ac96-683075706b8d",
-           "amenities":["Elevator", "Netflix"]
-         }'
